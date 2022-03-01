49acd4cd3b9fd47eee8742a5bc11562e36c35552
ilaoxucn <420648592@qq.com>
2022-03-01 11:28:54 +0800

    end-of-lesson68

--- recipes/forms.py
+++ recipes/forms.py
@@ -6,7 +6,7 @@ from .models import Recipe,RecipeIngredient
 class RecipeForm(forms.ModelForm):
     # required_css_class = 'required-field'
     # error_css_class = 'error-field'
-    name = forms.CharField(label='',widget=forms.TextInput(attrs={"class":"form-control","placeholder":"输入菜谱的名称"}))
+    name = forms.CharField(label='',help_text="菜谱名称字段的<a href='/contact'>联系我们</a>文字", widget=forms.TextInput(attrs={"class":"form-control","placeholder":"输入菜谱的名称"}))
     # description = forms.CharField(widget=forms.Textarea(attrs={"rows":3}))
     
     def __init__(self,*args,**kwargs):
